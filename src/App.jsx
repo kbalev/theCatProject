15d06f0c0745ebabd630f0db63b38ad1b66d8457
kbalev <kamen_balev@yahoo.com>
2021-07-06 17:30:13 +0100

    imagess not showing up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import './App.css';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Card from "./Card";
 
 
-const App = () =>{
-  return(
-    <Router>
-      <div>
-        <h1>The beginnings of the Cat website</h1>
-      </div>
+const App = () => {
+const [data, setData] = useState("");
+const [loading, setLoading] = useState(true)
+useEffect(
+  ()=>{
+    handleFetch()
+  }, []
+)
+  const handleFetch = async () =>{
+    const response = await fetch("https://api.thecatapi.com/v1/breeds")
+    const data = await response.json()
+    setData(data);
+    setLoading(false);
+  }
+  let cats = [...data];
+if (loading){
+  return <h1>Loading</h1>
+}
+return(
+  <Router>
+    <div>
+      <h1>The beginnings of the Cat website</h1>
+    </div>
+    <div>
+    <h1>Pulling a fetch request</h1>
+    <ul>
+      {cats.map((cat) => (
+        <Card id={cat.id} name={cat.name} description={cat.description} image={cat.image}/>
+      ))}
+    </ul>
+    <button onClick={handleFetch}>Click</button>
+  </div>
 
-    </Router>
-  )
+  </Router>
+)
 }
 
 export default App;
