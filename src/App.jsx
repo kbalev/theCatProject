1c61a46e0831a4f3c874356c0a546f12b97c5ec2
kbalev <kamen_balev@yahoo.com>
2021-07-06 12:21:06 +0100

    set up basics for building on top of

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+
+const App = () =>{
+  return(
+    <Router>
+      <div>
+        <h1>The beginnings of the Cat website</h1>
+      </div>
+
+    </Router>
+  )
+}
+
+export default App;
