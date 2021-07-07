be3c61dca48347e4ed4a51fb1af8e804849756c6
kbalev <kamen_balev@yahoo.com>
2021-07-07 11:49:39 +0100

    tidying up the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,31 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Card from "./Card";
-import Navbar from './components/Navbar';
-import Breed from './catbreed';
-
+import Navbar from "./components/Navbar";
+import Breed from "./catbreed";
 
 const App = () => {
-const [data, setData] = useState("");
-const [loading, setLoading] = useState(true)
-useEffect(
-  ()=>{
-    handleFetch()
-  }, []
-)
-  const handleFetch = async () =>{
-    const response = await fetch("https://api.thecatapi.com/v1/breeds")
-    const data = await response.json()
-    setData(data);
-    setLoading(false);
-  }
-  let cats = [...data];
-
-if (loading){
-  return <h1>Loading</h1>
-}
-return(
-  <Router>
-   <Navbar />
-  <nav>
+  return (
+    <Router>
+      <Navbar />
+      <nav>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='/catbreed'>Breed</Link>
+            <Link to="/catbreed">Breed</Link>
           </li>
         </ul>
       </nav>
-<Switch>
+      <Switch>
         <Route path="/catbreed">
           <Breed />
         </Route>
-        <Route exact path='/'>
-        </Route>
+        <Route exact path="/"></Route>
       </Switch>
-  </Router>
-)
-}
-
+    </Router>
+  );
+};
 
 export default App;
