5799b980a8197f39b5fab1241c63c97d2a79ca53
Carly Morris <thabai@hotmail.com>
2021-07-08 14:53:35 +0100

    rescue-home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Breed from './component-breeds/catbreed';
+import Rescue from './component-rescue/Rescue';
+import React from 'react';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
 
-const App = () => {
+const App  = () => {
+const[data, setData] = useState('');
+const [error, setError] = useState({error: false, message: ''});
+  
+      useEffect(() => {
+        handleFetch();
+  
+      }, []);
+  
+  const handleFetch = async () => {
+          try {
+          const response = await fetch('https://api.thecatapi.com/v1/breeds');
+   
+   
+          if (response.status !== 200) {
+            throw new error('Oops something went wrong')
+          }
+           const data = await response.json();
+           setData(data);    
+
+   
+       } catch (error) {
+         setError({error: true, message: error.message});
+       }
+     };
+     if (error.error) {
+      return <h1>An error has occured: {error.message}</h1>;
+    } else {
+      let cats= [...data];
+      const CatImage = cats
+        .filter((cat) => cat.image)
+        .map((cat) => {
+          return (
+              <div >
+                <img className='cat' src={cat.image.url} />
+                <h2>{cat.name}</h2>
+              </div>
+        );
+      });
+
+
   return (
     <Router>
       <nav>
@@ -14,6 +58,9 @@ const App = () => {
           <li>
             <Link to='/catbreed'>Breed</Link>
           </li>
+          <li>
+            <Link to='/Rescue'>Rescue</Link>
+          </li>
         </ul>
       </nav>
 
@@ -22,12 +69,17 @@ const App = () => {
         <Route path="/catbreed">
           <Breed />
         </Route>
+        <Route path="/Rescue">
+          <Rescue />
+        </Route>
         <Route exact path='/'>
         <h1>The beginnings of the Cat website</h1>
+         <Carousel>{CatImage}</Carousel>
         </Route>
       </Switch>
     </Router>
   );
+};
 };
 
 export default App;
---
+++ src/component-rescue/CatData.jsx
@@ -0,0 +1,44 @@
+let CatData = [
+  { name: "Billy", 
+    price: 80, 
+},
+  {
+    name: "George",
+    price: 40,
+  },
+  {
+    name: "Tiddles",
+    price: 50,
+  },
+  {
+    name: "Mittens",
+    price: 80,
+  },
+  {
+    name: "Nala",
+    price: 90,
+  },
+  {
+    name: "Chloe",
+    price: 80,
+  },
+  {
+    name: "Cleo",
+    price: 50,
+  },
+  {
+    name: "Rocky",
+    price: 40,
+  },
+  {
+    name: "Prince",
+    price: 60,
+  },
+  {
+    name: "Jinx",
+    price: 50,
+  },
+];
+
+
+export default CatData;
